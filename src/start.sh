5d0e67c4b7bd67f58316c8474a974f3cd5088d2b
adimpdl <ghag@mpdl.mpg.de>
2021-10-28 11:45:20 +0200

    keystore_added

--- src/start.sh
+++ src/start.sh
@@ -3,4 +3,4 @@
 #Extract NAT_IP and write to config
 
 echo "=====Starting from the script====="
-./Nethermind.Runner --config  /nethermind/configs/bloxberg.cfg --Network.ExternalIp ${NAT_IP} 
+./Nethermind.Runner --config  /nethermind/configs/bloxberg.cfg --Network.ExternalIp ${NAT_IP} --KeyStore.BlockAuthorAccount ${PUBLIC_ADDRESS} --KeyStore.UnlockAccounts ["${PUBLIC_ADDRESS}"] --KeyStore.EnodeAccount ${PUBLIC_ADDRESS}
