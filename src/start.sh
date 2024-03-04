858acaf5d603cb977f0fe750caac902de3ff0c06
aghag <ghag@mpdl.mpg.de>
2024-03-04 16:17:08 +0100

    nethermind version change

--- src/start.sh
+++ src/start.sh
@@ -2,5 +2,14 @@
 
 #Extract NAT_IP and write to config
 
-echo "=====Starting from the script====="
-./Nethermind.Runner --config  /nethermind/configs/bloxberg.cfg --Network.ExternalIp ${NAT_IP} --KeyStore.BlockAuthorAccount ${PUBLIC_ADDRESS} --KeyStore.UnlockAccounts ["${PUBLIC_ADDRESS}"] --KeyStore.EnodeAccount ${PUBLIC_ADDRESS}
+
+if [[ -z "${PUBLIC_ADDRESS}" ]]
+then
+  echo "=====Starting from the script inside if===== ${PUBLIC_ADDRESS}"
+  ./Nethermind.Runner --config  /nethermind/configs/bloxberg.cfg --Network.ExternalIp ${NAT_IP}
+else
+  echo "=====Starting from the script====="
+  ./Nethermind.Runner --config  /nethermind/configs/bloxberg.cfg --Network.ExternalIp ${NAT_IP} --KeyStore.BlockAuthorAccount ${PUBLIC_ADDRESS} --KeyStore.UnlockAccounts ["${PUBLIC_ADDRESS}"] --KeyStore.EnodeAccount ${PUBLIC_ADDRESS}
+fi
+
+
